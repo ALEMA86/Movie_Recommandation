02f8b47527e56be401f999dc25bbe7211ff6d636
ALEMA86 <aurore.lemaitre@gmail.com>
2021-11-16 11:30:02 +0100

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -96,8 +96,11 @@ def main():
         """
         Nous devons fournir à notre client les outils d’analyse de la base de données issue de __IMDB__.
                
-        Nous devons :
-
+        Il est nécessaire de :
+        - Pandas
+        - Sklearn
+        - Plotly
+        - Streamlit
         - Faire une rapide présentation de la base de données (sur notre espace collaboratif sur Github)
         - Fournir à notre client quelques statistiques sur les films :
             - Films : types, durées...
