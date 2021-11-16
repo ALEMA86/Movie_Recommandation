3ea9802c1c54b165562b28ab3b119249d9c21274
ALEMA86 <aurore.lemaitre@gmail.com>
2021-11-16 15:44:00 +0100

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -1,3 +1,9 @@
+######################################################################################
+######################################################################################
+###########################     LIBRAIRIES    ########################################
+######################################################################################
+######################################################################################
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -7,12 +13,43 @@ import datetime as dt
 import plotly.express as px
 import ipywidgets as widgets
 from plotly.subplots import make_subplots
+import seaborn as sns
 import plotly.graph_objects as go
 from sklearn.neighbors import NearestNeighbors
 
 
+######################################################################################
+######################################################################################
+###########################     DONNEES    ###########################################
+######################################################################################
+######################################################################################
+
 df_recommandation = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Database_projet/df_recommendation.csv?token=AU6BUZUA5UESEPKRRJQIESLBS53UU')
 df = pd.read_csv('https://raw.githubusercontent.com/BerengerQueune/ABC-Data/main/Berenger/Database_projet/df_base.csv?token=AU6BUZWHN456IAMFBUWFFSDBTELCU')
+country = pd.read_csv('https://github.com/ALEMA86/Movie_Recommandation/blob/79c19f676e356c1048fdb05d2b92574847b51960/Streamlit/Country.csv')
+######################################################################################
+######################################################################################
+###########################     FONCTIONS    #########################################
+######################################################################################
+######################################################################################
+
+@st.cache
+def load_df(url):
+    df = pd.read_csv(url)
+    df.set_index(df.iloc[:,0], inplace=True)
+    df = df.iloc[:, 1:]
+    return df
+
+
+
+
+
+######################################################################################
+######################################################################################
+###########################     INTERFACE    #########################################
+######################################################################################
+######################################################################################
+
 
 st.set_page_config( layout='wide')
 
@@ -323,7 +360,39 @@ def main():
 
         st.plotly_chart(fig)
 
+######################################################################################
+######################################################################################
+###########################     AURORE     ###########################################
+######################################################################################
+######################################################################################
 
+country_DF = load_df(country)
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.title(' ')
+        st.markdown(
+            """
+            Lors de notre analyse de la base de données, nous avons pu observer une grande variété de types d'oeuvres répertoriées par IMDb. 
+            
+            Notre cliente tenant un cinéma, nous nous sommes attachés à faire un focus sur les films, et avons retenu 2 types de films : les 'movie' et les 'tvMovie'.
+            
+            Il est cependant possible via le filtre ci-dessous de séparer ces 2 items afin de voir dans quels pays il y a le plus de 'movie de distribués, ou bien de 'tvMovie':
+            """
+        )
+        filter_type = st.multiselect(label='Selectionnez les types ?', options=list(country_DF.columns), default=['movie', 'tvMovie'], help="Seuls les 'movie' et 'tvMovie' ont été repris dans notre dataset pour des raisons d'hébergement des csv sur github.")
+    
+    country_DF_fil = pd.DataFrame(index=country_DF.index)
+    for col in filter_type:
+        if col in country_DF.columns:
+            country_DF_fil[col] = country_DF[col]
+
+    with col2:
+        fig = go.Figure()
+        for i in range(0, len(country_DF_fil.columns)):
+          sns.countplot(data = country_DF_fil, x= country_DF_fil.index, y= country_DF_fil.iloc[:, i], hue = country_DF_fil.columns[i], order = Films_by_Country_long.region.value_counts().iloc[:10].index)
+        st.plotly_chart(fig, use_container_width=True)
+    if show:
+        st.dataframe(country_DF)
 
 
 
