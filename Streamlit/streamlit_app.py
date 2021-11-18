9949805314c9f81cca0c3f0cd8fdbfae3fcc95d7
ALEMA86 <aurore.lemaitre@gmail.com>
2021-11-18 21:26:14 +0100

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -993,6 +993,27 @@ def main():
             
         st.plotly_chart(fig)
 
+
+        #####################################
+        st.title('Quels sont les films les mieux notés (+ de 8/10) - Caractéristiques communes ?')
+        qualify_movies2 = qualify_movies.copy()
+        qualify_movies2 = qualify_movies2[qualify_movies2['moyenne_ponderee'] >= 8 ]
+        qualify_movies2 = qualify_movies2[qualify_movies2['moyenne_ponderee'] <= 9 ]
+
+        fig = px.scatter_3d(qualify_movies2,x="genre1",y ='genre2', z= 'genre3', color = 'moyenne_ponderee'  )
+        fig.update_layout(title_text="Caractéristiques communes des films les mieux notés", title_x=0.5, width=1000, height=600, template='plotly_dark')
+            
+        st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
+        ####################################
         col1, col2 = st.columns([1, 1])
         with col1:
             st.title('Note moyenne par genre de films')
