f9a59abdfbfa2b61a420206752c4208dcc48fe29
ALEMA86 <aurore.lemaitre@gmail.com>
2021-11-18 14:54:23 +0100

    Update streamlit_app.py

--- Streamlit/streamlit_app.py
+++ Streamlit/streamlit_app.py
@@ -710,9 +710,9 @@ def main():
             #x_actress = Age_DF_clean[Age_DF_clean['category'].str.contains('actress')]
 
             fig = go.Figure()
-            fig.add_trace(go.Box(y= Age_DF_clean["Age"], x = Age_DF_clean[Age_DF_clean['category'].str.contains('actor')], name = 'Acteurs', marker_color='royalblue', boxmean=True # represent mean
+            fig.add_trace(go.Box(y= Age_DF_clean["Age"], name = 'actor', marker_color='royalblue', boxmean=True # represent mean
             ))
-            fig.add_trace(go.Box(y=Age_DF_clean["Age"], x= Age_DF_clean[Age_DF_clean['category'].str.contains('actress')], name = 'Actrices', marker_color='coral', boxmean=True # represent mean
+            fig.add_trace(go.Box(y=Age_DF_clean["Age"], name = 'actress', marker_color='coral', boxmean=True # represent mean
             ))
             #fig.update_yaxes(title= 'Age')
             #fig.update_xaxes(title= 'Population')
